00fd6d0ef2c7eb518cb9f777f278a986b8f09dba
yousefgh <yousefalghofili@hotmail.com>
2019-03-18 10:40:33 +0300

    optimization

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,36 @@
-$(document).ready(function() {
-     $('.img_container').mouseenter(function(){ 
-          $(this).find(".title").fadeOut(250);
+$(function () {
+     $('body')
+          .hide()
+          .fadeIn('slow');
+
+});
+
+$(document).ready(function () {
+     $('.img_container')
+          .mouseenter(function () {
+               $(this)
+                    .find(".title")
+                    .fadeOut(250);
+          });
+
+     $('.img_container').mouseleave(function () {
+          $(this)
+               .find(".title")
+               .fadeIn(250);
      });
 
-     $('.img_container').mouseleave(function(){ 
-          $(this).find(".title").fadeIn(250);
+     $('.img_container').mouseenter(function () {
+          $(this)
+               .find(".overlay")
+               .fadeIn(250);
+          $(this)
+               .find('.overlay')
+               .css('display', 'table');
      });
 
-     $('.img_container').mouseenter(function() {
-          $(this).find(".overlay").fadeIn(250);
-          $(this).find('.overlay').css('display', 'table');
-     });
-
-     $('.img_container').mouseleave(function() {
-          $(this).find(".overlay").fadeOut(250);
+     $('.img_container').mouseleave(function () {
+          $(this)
+               .find(".overlay")
+               .fadeOut(250);
      });
 });
